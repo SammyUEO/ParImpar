8b64e49ce6c61776eaea6afcc77510296c37066e
SammyUEO <146414814+SammyUEO@users.noreply.github.com>
2023-10-12 23:02:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ParImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+
+            x = Convert.ToInt32 (Console.ReadLine());
+            if (x%2 == 0)
+            {
+                Console.WriteLine("par");
+
+            }
+            else
+            {
+                Console.WriteLine("impar");
+            }
+        }
+    }
+}
